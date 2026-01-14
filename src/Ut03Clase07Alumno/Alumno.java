e4ef3cb87ff7131273690c3d7d5b8ec9f9cbe9ba
Natalia <natalia.marcos@educa.jcyl.es>
2026-01-14 07:12:46 +0100

    Finish

---
+++ src/Ut03Clase07Alumno/Alumno.java
@@ -0,0 +1,27 @@
+package Ut03Clase07Alumno;
+
+public class Alumno {
+
+    private String nombre;
+    private double nota;
+
+    public Alumno(String nombre, double nota) {
+        this.nombre = nombre;
+        this.nota = nota;
+    }
+
+    public double aprobar() {
+        if (nota < 5) {
+            nota = 5;
+        }
+        return nota;
+    }
+
+    public void imprimir() {
+        System.out.println(nombre + " tiene de nota " + nota);
+    }
+
+    public String toString() {
+        return nombre + " tiene de nota " + nota;
+    }
+}
