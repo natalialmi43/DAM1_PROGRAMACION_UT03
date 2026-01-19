1f5e9928ef2b36aec5ba174ef9f8a88528df924c
Natalia <natalia.marcos@educa.jcyl.es>
2026-01-19 20:55:29 +0100

    Finish

--- src/Ut03Ejercicios05BatallaDeRobots/Torneo.java
+++ src/Ut03Ejercicios05BatallaDeRobots/Torneo.java
@@ -4,11 +4,12 @@ public class Torneo {
     public static void main(String[] args) {
 
         Robot mazinger = new Robot("Mazinger", "metal");
-        Robot optimus = new Robot("Optimos", "madera");
+        Robot optimus = new Robot("Optimus", "madera");
 
         mazinger.mostrarEstadisticas();
         optimus.mostrarEstadisticas();
 
+        System.out.println("EMPIEZA LA PELEA:");
         while (mazinger.estaVivo() && optimus.estaVivo()){
             mazinger.atacar(optimus);
             if (optimus.estaVivo()){
@@ -19,11 +20,11 @@ public class Torneo {
         }
 
         if(mazinger.estaVivo()){
-            System.out.println("Mazinger esta vivo, y ha ganado");
+            System.out.println("Mazinger esta vivo, y ha ganado!!!");
         }
 
         if (optimus.estaVivo()){
-            System.out.println("Optimus está vivo, y ha ganado");
+            System.out.println("Optimus está vivo, y ha ganado!!!");
         }
     }
 }
